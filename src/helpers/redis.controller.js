b6dd080407c17748a207fa4318ae596b97a84262
Marco Antonio Gallardo <magallope@gmail.com>
2022-10-02 12:02:34 +0200

    FIX cambiado modo de conexion de redis para hacerlo compatible con RailWay

--- src/helpers/redis.controller.js
+++ src/helpers/redis.controller.js
@@ -1,8 +1,7 @@
 const { createClient } = require("redis");
 
 const redisClient = createClient({
-    host:process.env.REDIS_HOST,
-    port:process.env.REDIS_PORT
+    url: process.env.REDIS_URL || null
 });
 
 (async () => {
